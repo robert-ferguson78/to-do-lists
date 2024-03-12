d068b28a30a6c1d8a0aee78363b37733d1166b05
Robert Ferguson <robertferguson78@gmail.com>
2024-03-12 16:11:39 +0000

    Exercise 1 - add task with date columns

---
+++ to-do-list-v1/todo.js
@@ -0,0 +1,20 @@
+let todoItems = [];
+
+function renderTodo(todo) {
+  const table = document.getElementById("todo-table");
+  const row = table.insertRow(-1);
+  const textCell = row.insertCell(0);
+  textCell.innerText = todo.text;
+  const dateCell = row.insertCell(1);
+  dateCell.innerText = todo.textDate;
+}
+
+function addTodo() {
+  const todoText = document.getElementById("todo-id").value;
+  const todo = {
+    text: todoText,
+    textDate: new Date().toLocaleString()
+  };
+  todoItems.push(todo);
+  renderTodo(todo);
+}
